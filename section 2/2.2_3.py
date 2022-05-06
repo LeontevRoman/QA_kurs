c5c83d688514aed2da779c61cc0af441fe6dbd32
Roman Leontev <roman.l.v@yandex.ru>
2022-05-06 18:07:24 +0300

    Select value drop-down list

--- section 2/2.2_3.py
+++ section 2/2.2_3.py
@@ -3,22 +3,21 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
 
-
 try:
     browser = webdriver.Chrome()
     browser.get('http://suninjuly.github.io/selects1.html')
 
     a = browser.find_element(By.ID, 'num1').text
     b = browser.find_element(By.ID, 'num2').text
-    summa = int(a) + int(b)
+    value = int(a) + int(b)
 
     find = Select(browser.find_element(By.ID, 'dropdown'))
-    find.select_by_value(str(summa))
+    find.select_by_value(str(value))
 
     button = browser.find_element(By.CSS_SELECTOR, '[type="submit"]')
     button.click()
-    print(summa)
+    # print(value)
 
 finally:
-    time.sleep(10)
+    time.sleep(5)
     browser.quit()
