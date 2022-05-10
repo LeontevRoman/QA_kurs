70f58ee76cc2fd179021bea7f89cb3ca59aef748
Roman Leontev <roman.l.v@yandex.ru>
2022-05-10 19:59:12 +0300

    final test

--- section 3/3_2_13 (link unit test)/TestLinkUnit.py
+++ section 3/3_2_13 (link unit test)/TestLinkUnit.py
@@ -1,16 +1,33 @@
 import unittest
-from Link import Link
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def link(linked):
+    browser = webdriver.Chrome()
+    browser.get(linked)
+
+    browser.find_element(By.XPATH, "//input[@placeholder='Input your first name']").send_keys("Ivan")
+    browser.find_element(By.XPATH, "//input[@placeholder='Input your last name']").send_keys("Petrov")
+    browser.find_element(By.XPATH, "//input[@placeholder='Input your email']").send_keys('Smolensk')
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    return welcome_text_elt.text
+    browser.quit()
 
 
 class TestLink(unittest.TestCase):
-    def setUp(self):
-        self.Link = Link()
 
     def test_link1(self):
-        self.assertEqual(self.Link.link_1(), "Congratulations! You have successfully registered!")
+        link1 = 'http://suninjuly.github.io/registration1.html'
+        self.assertEqual(link(link1), "Congratulations! You have successfully registered!")
 
     def test_link2(self):
-        self.assertEqual(self.Link.link_2(), "Congratulations! You have successfully registered!")
+        link2 = 'http://suninjuly.github.io/registration2.html'
+        self.assertEqual(link(link2), "Congratulations! You have successfully registered!")
 
 
 if __name__ == '__main__':
